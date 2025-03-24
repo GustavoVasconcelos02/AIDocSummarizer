4dd46ecbdc3e8101deed91773ea99aa7a9f8560c
Gustavo Serra Vasconcelos <82975470+GustavoVasconcelos02@users.noreply.github.com>
2025-03-24 09:27:59 -0400

    chore: adicionar questões para orientar criação de resumos finais jurídicos

---
+++ app/utils/questions.py
@@ -0,0 +1,14 @@
+# Questões para orientar a criação dos resumos finais
+SUMMARY_QUESTIONS = """
+Qual recurso está sendo julgado (recurso extraordinário ou agravo)?
+Qual órgão julgador proferiu o acórdão recorrido?
+Houve reforma ou confirmação de decisão anterior?
+O acórdão recorrido foi proferido por unanimidade de votos ou por maioria?
+Quais são os fundamentos apresentados pelo relator?
+Qual é a transcrição literal da ementa?
+Qual foi o juízo de admissibilidade do recurso extraordinário (admissão ou inadmissão) e quais os seus fundamentos (matéria infraconstitucional, súmula 279 etc.)?
+O recurso extraordinário foi interposto com fundamento em qual dispositivo constitucional (art. 102, III, a, b, c ou d, da CF)?
+Quais os dispositivos indicados como violados e quais os argumentos relevantes do recurso?
+Quais os pedidos formulados no recurso?
+Há contrarrazões e quais são os argumentos relevantes do recurso?
+"""
